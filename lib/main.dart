e1461c34d56e9d3291ef046dcc022b6366fb5190
Denisha555 <firdiantidenisha05@gmail.com>
2025-03-30 19:52:18 +0700

    Simpan perubahan

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'screens/daftar.dart';
 import 'screens/masuk.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
